46d083d2bfd4bd21e1449b0f77fe17377c9f6a47
Pant <abpant@deloitte.com>
2016-03-09 12:02:07 +0530

    With sum of sequence number

--- sumOfSequence.py
+++ sumOfSequence.py
@@ -1,1 +1,15 @@
 __author__ = 'abpant'
+# to get a comma seperated list of nubers and to find the sum
+listOfNumbers=list();
+inpStr=raw_input("PLease enter the comma seperated input list.");
+listOfNumbers= inpStr.split(",");
+print listOfNumbers
+
+itr=0;
+sum=0;
+
+while itr<len(listOfNumbers):
+    sum=sum+float(listOfNumbers[itr]);
+    itr=itr+1;
+
+print sum;
